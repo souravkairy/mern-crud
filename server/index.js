feddd048454a470c5ec2b72738bd1d5a948ac36b
souravkairy <souravkairy.se@gmail.com>
2023-11-22 01:26:56 +0600

    get and post api done

---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.status(234).send("get done");
+});
+
+app.use(express.json());
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App is listening to port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.post("/book", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({ message: "set all required field" });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    return response.status(500).send({ message: error.message });
+  }
+});
